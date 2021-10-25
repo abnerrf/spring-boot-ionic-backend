cc5f9563b42565a72188a741c73b388cb371eb7c
abnerrf <abnerrf@hotmail.com>
2021-10-25 15:06:43 -0300

    Validação customizada: E-mail não repetido na atualização de Cliente

--- src/main/java/com/abnerrodrigues/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/abnerrodrigues/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.abnerrodrigues.cursomc.domain.Cliente;
+import com.abnerrodrigues.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
